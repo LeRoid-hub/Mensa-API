873237d3d2f197d1303c5a85b2dd4976605e1730
Jan Barfuß <janbar2001@gmail.com>
2023-12-20 14:03:16 +0100

    scraper

--- src/stripper.ts
+++ src/stripper.ts
@@ -1,3 +1,14 @@
+import * as cheerio from 'cheerio';
+
 export default function stripHtml(html: string): string {
-    return html;
+
+    const $ = cheerio.load(html);
+
+    const $mensen  = $('.group').find('.element');
+    console.log($mensen.text() + " length: " + $mensen.length);
+
+    const elements = $('.elements:eq(0)');
+    console.log(elements.text());
+    return $.text();
 }
+
