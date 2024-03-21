62577d4c41ae61ed3a5ba8f08abf733d90ea6816
Jan Barfuß <janbar2001@gmail.com>
2024-03-21 19:32:40 +0100

    Some Structure

---
+++ cache/cache.go
@@ -0,0 +1,18 @@
+package cache
+
+import (
+	"mensa/types"
+)
+
+func NewCache() *types.Cache {
+	cache := types.Cache{}
+	return &cache
+}
+
+func setCacheData(cache *types.Cache, key string, data string, lifetime ...int64) {
+	cache.SetData(key, data, lifetime...)
+}
+
+func getCacheData(cache *types.Cache) string {
+	return cache.GetData()
+}
